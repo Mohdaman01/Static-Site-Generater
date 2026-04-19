ef0897a74a28415055025a69d53437709dce702a
Mohdaman01 <mohdastar200026@gmail.com>
2026-04-19 17:13:11 +0530

    done changes for github pages

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    args = sys.argv[1:]
+    basePath = args[0]
+    if basePath is None:
+        basePath = "/"
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -19,7 +25,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basePath)
 
 
 main()
